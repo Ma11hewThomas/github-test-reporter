263a8e9b4e14b4c51ea900c86a8bdb5c09381146
Matthew Thomas <matthewthomasgb@gmail.com>
2025-03-09 10:30:29 +0000

    wip

--- src/handlebars/helpers/ctrf.ts
+++ src/handlebars/helpers/ctrf.ts
@@ -127,6 +127,10 @@ export function formatDurationStartStopToHumanHelper(): void {
       return 'not captured'
     }
 
+    if (isNaN(start) || isNaN(stop)) {
+      return 'not captured'
+    }
+
     const durationMs = stop - start
     if (durationMs < 1) {
       return `1ms`
